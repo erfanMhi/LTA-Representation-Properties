cf5a1a47bb902f1a092cdf4f464875b28cce6397
Erfan Miahi <mhi.erfan1@gmail.com>
2021-06-24 15:06:38 -0600

    saving images is removed.

--- core/agent/base.py
+++ core/agent/base.py
@@ -10,6 +10,8 @@ class Agent:
         self.cfg = cfg
         self.episode_reward = 0
         self.episode_rewards = []
+        self.image_array = [] # This one is needed for creating the videos
+        self.record_video = self.cfg.record_video
         self.total_steps = 0
         self.reset = True
         self.ep_steps = 0
@@ -24,11 +26,15 @@ class Agent:
         self.step_reward = np.zeros(self.timeout)
 
     def update_stats(self, reward, done):
+        
         self.step_reward[self.ep_steps] = reward
 
         self.episode_reward += reward
         self.total_steps += 1
         self.ep_steps += 1
+        if self.record_video:
+            self.image_array.append(self.state)
+        
         if done or self.ep_steps == self.timeout:
             if self.ep_steps < self.timeout:
                 self.step_reward[self.ep_steps:] = -3
@@ -42,24 +48,29 @@ class Agent:
                 self.add_episode_return(self.ep_steps)
             else:
                 raise NotImplementedError
+            
             self.episode_reward = 0
             self.ep_steps = 0
             self.reset = True
 
-            if (self.num_episodes <= 20  or self.total_steps >= self.cfg.max_steps*(1-0.01)) and \
-                self.num_episodes % 10 == 0:
-                # cmap = cm.get_cmap('viridis', 5)
-                cmap = (mpl.colors.ListedColormap(["white", 'red', 'cyan', 'grey', 'yellow', ]))
-                plt.figure()
-                temp = self.step_reward.reshape((25, 20))
-                plt.imshow(temp, cmap=cmap, vmax=1.5, vmin=-3.5)
-                plt.colorbar()
-                viz_dir = self.cfg.get_visualization_dir()
-                viz_file = 'reward_ep{}.png'.format(self.num_episodes)
-                plt.savefig(os.path.join(viz_dir, viz_file))
-                plt.close()
-                plt.clf()
-                print("Save reward plot in {}".format(viz_file))
+            if self.record_video:
+                self.cfg.logger.log_video(self.image_array, len(self.episode_rewards), 10, (150, 150, 3))
+                del self.image_array
+                self.image_array = []
+#             if (self.num_episodes <= 20  or self.total_steps >= self.cfg.max_steps*(1-0.01)) and \
+                # self.num_episodes % 10 == 0:
+                # # cmap = cm.get_cmap('viridis', 5)
+                # cmap = (mpl.colors.ListedColormap(["white", 'red', 'cyan', 'grey', 'yellow', ]))
+                # plt.figure()
+                # temp = self.step_reward.reshape((25, 20))
+                # plt.imshow(temp, cmap=cmap, vmax=1.5, vmin=-3.5)
+                # plt.colorbar()
+                # viz_dir = self.cfg.get_visualization_dir()
+                # viz_file = 'reward_ep{}.png'.format(self.num_episodes)
+                # plt.savefig(os.path.join(viz_dir, viz_file))
+                # plt.close()
+                # plt.clf()
+                # print("Save reward plot in {}".format(viz_file))
             self.step_reward = np.zeros(self.timeout)
 
     def add_episode_return(self, ep_return):
@@ -92,6 +103,7 @@ class Agent:
 
     def eval_episodes(self, elapsed_time=None):
         eval_res = []
+        
         for ep in range(self.cfg.eval_episodes):
             ep_return, steps = self.eval_episode()
             if self.cfg.evaluation_criteria == "return":
@@ -100,15 +112,18 @@ class Agent:
                 eval_res.append(steps)
             else:
                 raise NotImplementedError
+
         eval_res = np.array(eval_res)
         mean, median, min, max = np.mean(eval_res), np.median(eval_res), np.min(eval_res), np.max(eval_res)
 
         total_episodes = len(self.episode_rewards)
         log_str = 'EVAL: total steps %d, total episodes %3d, ' \
                   'returns %.2f/%.2f/%.2f/%.2f/%d (mean/median/min/max/num), %.2f steps/s'
+
         self.cfg.logger.info(log_str % (self.total_steps, total_episodes, mean, median,
                                         min, max, len(eval_res),
                                         elapsed_time))
+        
         self.episode_reward = 0
         self.ep_steps = 0
         self.reset = True
@@ -118,6 +133,7 @@ class Agent:
         state = self.env.reset()
         total_rewards = 0
         ep_steps = 0
+        
         while True:
             action = self.eval_step(state)
             state, reward, done, _ = self.env.step([action])
@@ -125,6 +141,7 @@ class Agent:
             ep_steps += 1
             if done or ep_steps == self.cfg.timeout:
                 break
+        
         return total_rewards, ep_steps
 
     def policy(self, state, eps):

c334f0e0220c5490097e8dfc62a073e1161a661f
Erfan Miahi <mhi.erfan1@gmail.com>
2022-03-14 12:18:35 -0600

    the GPU experiments are now fully deterministic.

--- core/utils/torch_utils.py
+++ core/utils/torch_utils.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+import random
 import torch
 
 
@@ -30,6 +31,11 @@ def to_np(t):
 def random_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
 
 
 def set_one_thread():
